e948670e0fcef17ac530d4412815958a6128f004
임시온 <80656700+dlatldhs@users.noreply.github.com>
2022-08-01 21:59:28 +0900

    Add files via upload

--- tensorflow/app.py
+++ tensorflow/app.py
@@ -1,10 +1,13 @@
-from distutils.command.config import config
-from email.policy import default
+
+####
+import flask ## 
+from flask import request
 from flask import Flask , render_template , url_for
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import PIL
+from flask import request
 import tensorflow as tf
 
 from tensorflow import keras
@@ -15,21 +18,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('gp.html')
 
-@app.route('/test')
-def test():
+@app.route('/gp',methods=['GET','POST'])
+def result():
+    if request.method == 'POST':
+        val = request.form
+        return render_template("ttt.html",result=val)
+    else :
+        return render_template("ttt.html")
+
+@app.route('/gpp', methods=['GET','POST'])
+def result1():
+    return str(request.form)
+
+@app.route('/test2')
+def test2():
     return render_template('test.html')
 
 @app.route('/exam')
 def predict():
+
+    # variable
     img_height = 180
     img_width = 180
     class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+
+    # load model
     model = tf.keras.models.load_model('model_save2.h5')
+
+    # test용 필요한 것들
     rose_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Garden_roses_%28yellow-red%29.jpg/800px-Garden_roses_%28yellow-red%29.jpg"
-    Tulip_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/1200px-Sunflower_sky_backdrop.jpg"
-    rose_path =  tf.keras.utils.get_file('Sunflower', origin=Tulip_url)
+    rose_path =  tf.keras.utils.get_file('Sunflower', origin=rose_url)
     img = tf.keras.utils.load_img(
         rose_path, target_size=(img_height, img_width)
         )
@@ -37,13 +57,17 @@ def predict():
     img_array = tf.expand_dims(img_array,0)
     predictions = model.predict(img_array)
     score = tf.nn.softmax(predictions[0])
+    
+    # 프린트
     print(
         "This image most likely belongs to {} with a {:.2f} percent confidence."
         .format(class_names[np.argmax(score)], 100 * np.max(score))
     )
+    
+    # 결과 html에 전달하기
     Textttt = "This image most likely belongs to {} with a {:.2f} percent confidence.".format(class_names[np.argmax(score)], 100 * np.max(score))
     
     return Textttt
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8080)
